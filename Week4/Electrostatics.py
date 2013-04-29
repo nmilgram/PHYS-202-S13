53178043fbb37b8799c3a220d137c7a08e523432
nmilgram <nmilgram@csm-ru-dirac-1a.cp-calpoly.edu>
2013-04-28 23:38:56 -0700

    3

--- Week4/Electrostatics.py
+++ Week4/Electrostatics.py
@@ -1,5 +1,7 @@
-def dipolePotential(x,y,q,d):
-    return (k*q/((X-d)**2+Y**2)**(1./2.))+(k*q/((X+d)**2+Y**2)**(1./2.))
+def pointPotential(X,Y,q,posx,posy):
+	k=8.987551787
+	return k*q/((X-posx)**2 + (Y-posy)**2)**(1./2.)
 
-def pointPotential(x,y,q,posx,posy):
-    (k*q/((X-posx)**2+(Y-posy)**2)**(1./2.))
+def dipolePotential(X,Y,q,d):
+	k=8.987551787
+	return (k*q/((X-d)**2 + Y**2)**(1./2.))-(k*q/((X+d)**2 + Y**2)**(1./2.))
