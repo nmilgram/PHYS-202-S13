aef5440757f27a7eed6d65a234a757751324707e
Nathaniel Milgram <nmilgram@csm-pm-38123-04.local>
2013-05-07 16:02:32 -0700

    m

---
+++ Miderm/pledge.md
@@ -0,0 +1,6 @@
+I have read and understand the rules regarding academic honesty and I agree to abide by them.
+I pledge on my honor that I have not given or received any unauthorized assistance on assignments or ex-
+aminations in this course.
+
+Signed: Nathaniel Milgram
+Dated: 5/7/13
--- Week4/Electrostatics.py
+++ Week4/Electrostatics.py
@@ -5,3 +5,8 @@ def pointPotential(X,Y,q,posx,posy):
 def dipolePotential(X,Y,q,d):
 	k=8.987551787
 	return (k*q/((X-d)**2 + Y**2)**(1./2.))-(k*q/((X+d)**2 + Y**2)**(1./2.))
+
+def pointField(x,y,q,Xq,Yq):
+	Ex=k*q*(x-Xq)/((x-Xq)**2.+(y-Yq)**2)**(.5)
+	Ey=k*q*(y-Yq)/((x-Xq)**2.+(y-Yq)**2)**(.5)      
+	return Ex, Ey
